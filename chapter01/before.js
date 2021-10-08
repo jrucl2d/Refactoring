45927ffe7bc91f23c9cb891156a2866f7e5fd1bd
jrucld <ysk789465@gmail.com>
2021-10-08 23:35:08 +0900

    play를 함수로 추출, 인라인으로 처리해서 매개변수 줄이기

--- chapter01/before.js
+++ chapter01/before.js
@@ -12,15 +12,14 @@ function statment(invoice, plays) {
     }).format;
 
     for (let perf of invoice.perfomances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
+        let thisAmount = amountFor(perf);
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트 제공
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역 출력
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
@@ -28,25 +27,29 @@ function statment(invoice, plays) {
     return result;
 }
 
-function amountFor(perf, play) {
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy":
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
 }
