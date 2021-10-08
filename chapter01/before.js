d2c9746b4fa7e00c673d3d78d21e4fafd82a2021
jrucld <ysk789465@gmail.com>
2021-10-09 00:06:44 +0900

    반복문 쪼개기로 volumeCredits 반복분 하나 더 추가, 문장 슬라이드 후 함수 추출, 변수 인라인하기

--- chapter01/before.js
+++ chapter01/before.js
@@ -3,19 +3,24 @@ const plays = require('./plays.json')
 
 function statment(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.perfomances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // 청구 내역 출력
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
     return result;
+}
+
+function totalVolumeCredits(invoice) {
+    let volumeCredits = 0;
+    for (let perf of invoice.perfomances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
 }
 
 function usd(aNumber) {
