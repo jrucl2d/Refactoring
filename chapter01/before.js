e65498eb12f8d4d7a29e31b3be21b6436ad0f0a1
jrucld <ysk789465@gmail.com>
2021-10-10 20:59:16 +0900

    중간 데이터에 invoice의 정보들 넣고 invoice 매개변수에서 삭제

--- chapter01/before.js
+++ chapter01/before.js
@@ -2,13 +2,16 @@ const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
 function statment(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.perfomances = invoice.perfomances;
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.perfomances) {
+    for (let perf of data.perfomances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
     result += `총액: ${usd(totalAmount())}\n`;
@@ -17,7 +20,7 @@ function renderPlainText(invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.perfomances) {
+        for (let perf of data.perfomances) {
             result += amountFor(perf);
         }
         return result;
@@ -25,7 +28,7 @@ function renderPlainText(invoice, plays) {
     
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.perfomances) {
+        for (let perf of data.perfomances) {
             result += volumeCreditsFor(perf);
         }
         return result;
