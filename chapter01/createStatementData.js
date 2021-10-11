7b0ffef1ad1b295cd28cc1a059c2497205084090
jrucld <ysk789465@gmail.com>
2021-10-11 21:48:34 +0900

    함수 선언 바꾸기로 클래스 내에 play 전달

--- chapter01/createStatementData.js
+++ chapter01/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+        this.perfomance = aPerformance;
+        this.play = aPlay;
+    }
+}
+
 function createStatementdata(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,8 +14,9 @@ function createStatementdata(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-        result.play = playFor(result);
+        result.play = calculator.play;
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
         return result;
