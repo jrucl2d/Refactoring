75abdc467524bfe23a45d9b2e61944bfaafa90de
jrucld <ysk789465@gmail.com>
2021-10-16 20:48:24 +0900

    테스트 추가하기, beforeEach 사용

--- chapter04/test.js
+++ chapter04/test.js
@@ -1,11 +1,18 @@
-const { assert, expect } = require("chai");
-const { describe, it } = require("mocha");
+const { expect } = require("chai");
+const { describe, it, beforeEach } = require("mocha");
 const { Province, sampleProvinceData } = require("./code");
 
 describe('province', function() {
+    let asia;
+    beforeEach(function() {
+        asia = new Province(sampleProvinceData());
+    })
+
     it('shortfall', function() {
-        const asia = new Province(sampleProvinceData());
-        // assert.equal(asia.shortfall, 5);
         expect(asia.shortfall).equal(5);
+    })
+    
+    it('profit', function() {
+        expect(asia.profit).equal(230);
     })
 })
