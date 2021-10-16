14055f9ef7dc39613495565880bcfb486024ff84
jrucld <ysk789465@gmail.com>
2021-10-16 20:39:28 +0900

    chai 추가

--- chapter04/test.js
+++ chapter04/test.js
@@ -1,10 +1,11 @@
-const assert = require("assert"); 
+const { assert, expect } = require("chai");
 const { describe, it } = require("mocha");
 const { Province, sampleProvinceData } = require("./code");
 
 describe('province', function() {
     it('shortfall', function() {
         const asia = new Province(sampleProvinceData());
-        assert.equal(asia.shortfall, 5);
+        // assert.equal(asia.shortfall, 5);
+        expect(asia.shortfall).equal(5);
     })
 })
