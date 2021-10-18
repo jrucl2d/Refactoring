76ab1cd032a9c612f08df9a9cc43df6281193312
jrucld <ysk789465@gmail.com>
2021-10-18 21:06:11 +0900

    클래스 내부에서 변수 추출 시에는 메소드로 추출

--- chapter06/price.js
+++ chapter06/price.js
@@ -14,8 +14,11 @@ class Order {
     get itemPrice() {return this._data.itemPrice;}
 
     get price() {
-        return this.quantity * this.itemPrice -
-            Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+        return this.basePrice - this.quantityDiscount + this.shipping;
     }
+    
+    // 클래스 전체에 영향을 주면 변수가 아닌 메소드로 추출
+    get basePrice() {return this.quantity * this.itemPrice;}
+    get quantityDiscount() {return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;}
+    get shipping() {return Math.min(this.quantity * this.itemPrice * 0.1, 100);}
 }
