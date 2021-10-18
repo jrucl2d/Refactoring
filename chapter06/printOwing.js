4519e8989a535d8768f81a353feb6ac9a8ef3dff
jrucld <ysk789465@gmail.com>
2021-10-18 20:50:55 +0900

    변수 대입이 있을 시 분리 함수에서 해당 값을 리턴하도록

--- chapter06/printOwing.js
+++ chapter06/printOwing.js
@@ -1,9 +1,8 @@
 function printOwing(invoice) {
-    let outstanding = 0;
-
     printBanner();
 
-    // 미해결 채무(outstanding)를 계산`
+    // 미해결 채무(outstanding)를 계산
+    let outstanding = 0;
     for (const o of invoice.orders) {
         outstanding += o.amount;
     }
