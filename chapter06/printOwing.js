308b30bbb97ba31adf06f872b7c9b529092bae69
jrucld <ysk789465@gmail.com>
2021-10-18 20:48:49 +0900

    함수 추출 전

---
+++ chapter06/printOwing.js
@@ -0,0 +1,22 @@
+function printOwing(invoice) {
+    let outstanding = 0;
+
+    printBanner();
+
+    // 미해결 채무(outstanding)를 계산
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    // 마감일(dueDate)을 기록
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    printDetails(invoice, outstanding);
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`고객명 : ${invoice.customer}`);
+    console.log(`채무액 : ${outstanding}`);
+    console.log(`마감일 : ${invoice.dueDate.toLocalDateString()}`);
+}
