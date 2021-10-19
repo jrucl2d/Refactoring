e829a759bf6686c5078570f47a2bd295401ee2b8
jrucl2d <ysk789465@gmail.com>
2021-10-19 15:18:48 +0900

    기존 데이터를 객체로 만들기

--- chapter07/code.js
+++ chapter07/code.js
@@ -1,8 +1,18 @@
-const organization = {name: "애크미 구스베리", country: "GB"};
+// const organization = {name: "애크미 구스베리", country: "GB"};
 
-function getRawDataOfOrganization() {
-    return organization;
+// function getRawDataOfOrganization() {
+//     return organization;
+// }
+
+// const result = `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
+// getRawDataOfOrganization().name = "new name"; // 쓰기 예
+
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
 }
 
-const result = `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
-getRawDataOfOrganization().name = "new name"; // 쓰기 예
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+function getRawDataOfOrganization() {return organization.data;}
+function getOrganization() {return organization;}
