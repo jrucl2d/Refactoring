10c77842f8e7b2d07efb12c9ab0318676626c7ac
jrucl2d <ysk789465@gmail.com>
2021-10-19 15:22:03 +0900

    객체 내부에 getter, setter 추가. 변수 펼치기

--- chapter07/code.js
+++ chapter07/code.js
@@ -9,10 +9,17 @@
 
 class Organization {
     constructor(data) {
-        this._data = data;
+        this._name = data.name;
+        this._country = data.country;
     }
+    set name(aString) {this._name = aString;}
+    get name() {return this._name;}
+    set country(aCountryCode) {this._country = aCountryCode;}
+    get country() {return this._country;}
 }
 
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
-function getRawDataOfOrganization() {return organization.data;}
 function getOrganization() {return organization;}
+
+const result = `<h1>${getOrganization().name}</h1>`; // getter를 사용해 읽기
+getOrganization().name = "new name"; // setter를 사용해 쓰기
